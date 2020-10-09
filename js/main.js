2d6f1146304262a429c389c1eb070849f93582c2
minusvetal <68374186+minusvetal@users.noreply.github.com>
2020-10-09 15:45:31 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,11 @@
+new WOW().init();
+const cartButton = document.querySelector("#cart-buttons");
+const modal = document.querySelector(".modal");
+const close = document.querySelector(".close");
+
+cartButton.addEventListener("click", toggleModal);
+close.addEventListener("click", toggleModal);
+
+function toggleModal() {
+  modal.classList.toggle("is-open");
+}
